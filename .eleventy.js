131d9c0199c100bd134f1b1403a295321574e9da
CORPORATE\ckeh <harry.kehagioglou@iteam.gr>
2021-05-31 13:58:14 +0300

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+module.exports = function (eleventyConfig) {
+    /* eleventyConfig.setTemplateFormats([
+      "jpg",
+      "eot",
+      "svg",
+      "ttf",
+      "woff",
+      "woff2"
+    ]); */
+
+    eleventyConfig.addWatchTarget("assets/sass/");
+    eleventyConfig.addPassthroughCopy("assets/images/");
+    eleventyConfig.addPassthroughCopy("assets/css");    
+    eleventyConfig.addPassthroughCopy("assets/js");
+    eleventyConfig.addPassthroughCopy("assets/webfonts");
+    
+  
+    // Filter to retrieve a random blog post
+    /* eleventyConfig.addFilter("randomPost", (arr) => {
+      arr.sort(() => {
+        return 0.5 - Math.random();
+      });
+      return arr.slice(0, 1);
+    }); */
+  
+    return {
+      dir: {
+        input: "src",
+        output: "dist",
+      },
+    };
+  };
